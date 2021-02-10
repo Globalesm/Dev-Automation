8263bf6ac0c6b9725ae74c9270bb3cb14b8a104b
Giovanni <giogiannola@globalesm.com>
2021-02-09 20:38:12 -0500

    DHS-OAST-2020-2021 Commit
    DHS-OAST-2020-2021 Commit

--- examples/axe-core/axe-advanced-project1/config/01-custom-axe.config.js
+++ examples/axe-core/axe-advanced-project1/config/01-custom-axe.config.js
@@ -1,48 +1,4 @@
-#!/usr/bin/env node
-'use strict';
-
-//
-//*** THIS VERSION RUNS TESTS AGAINST ALL AXE RULES - NONE ARE DISABLED
-//
-
-
-const customAxe = require('..');
-const pkg = require('../package.json');
-const globby = require('globby');
-const protocolify = require('protocolify');
-const commander = require('commander');
-
-const fs = require('fs');
-
-commander
-	.version(pkg.version)
-	.name('node custom-axe.js')
-	.usage('[options] <paths>')
-	.option(
-		'-s, --sitemap <url>',
-		'the path to a sitemap'
-	)
-	.option(
-		'-f, --sitemap-find <pattern>',
-		'a pattern to find in sitemaps. Use with --sitemap-replace'
-	)
-	.option(
-		'-r, --sitemap-replace <string>',
-		'a replacement to apply in sitemaps. Use with --sitemap-find'
-	)
-	.option(
-		'-x, --sitemap-exclude <pattern>',
-		'a pattern to find in sitemaps and exclude any url that matches'
-	)
-	.requiredOption(
-		'-h, --html-report <dir>',
-		'Takes json output and uses pa11y-ci-reporter-html to generate a report in <dir>'
-	).parse(process.argv);
-
-// Parse the args into valid paths using glob and protocolify
-let commandLineUrls = globby.sync(commander.args, {nonull: true}).map(protocolify);
-
-const config = {
+const config1 = {
 	urls: [
 		// Hard-code URLs for testing here
 
@@ -187,27 +143,4 @@ const config = {
 	}
 };
 
-const main = async () => {
-	// configuration urls first, then commandline urls then sitemap
-	config.urls = config.urls.concat(commandLineUrls);
-
-	const filteredSitemapUrls = await customAxe.retrieveSitemapUrls(commander.sitemap,
-		commander.sitemapFind,
-		commander.sitemapReplace,
-		commander.sitemapExclude);
-
-	config.urls = config.urls.concat(filteredSitemapUrls);
-
-	const results = await customAxe.scanUrls(config.urls, config.axeConfig);
-
-	const summaryResults = await customAxe.convertAxeResultsToPa11yReportCompatible(results);
-	// console.log(JSON.stringify(summaryResults, null, 2));
-
-	await customAxe.generateHtmlReports(summaryResults, commander.htmlReport, {});
-};
-
-main().catch(error => {
-	console.error('unexpected failure: ');
-	console.error(error);
-	process.exit(1);
-});
+module.exports = { config1 };
---
+++ examples/axe-core/axe-advanced-project1/config/index.js
@@ -0,0 +1,5 @@
+const { config1 } = require('./01-custom-axe.config');
+const { config2 } = require('./02-custom-axe.config');
+const { config } = require('./custom-axe.config');
+
+module.exports = { config1, config2, config };
